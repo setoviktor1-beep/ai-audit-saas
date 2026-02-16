6b369c74694e9a6ab50fc51017ea6df1b8aa462b
setoviktor1-beep <setoviktor1@gmail.com>
2026-02-16 08:38:19 +0100

    Fix Stripe API version

--- lib/stripe/client.ts
+++ lib/stripe/client.ts
@@ -9,7 +9,6 @@ export function getStripeClient() {
 
   if (!stripeClient) {
     stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2025-10-29.clover',
       typescript: true,
     });
   }
